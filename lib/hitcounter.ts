a1399c0174e25a4c0a5a75cf7db83ce8ae8ca9be
LuanComputacao <eng.luanroger@gmail.com>
2023-07-24 01:33:24 -0300

    feat: add readCapacity to the hitcounter

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -4,40 +4,55 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import {Construct} from 'constructs';
 
 export interface HitCounterProps {
-    /** the function for which we want to count url hits **/
-    downstream: lambda.IFunction;
+  /** the function for which we want to count url hits **/
+  downstream: lambda.IFunction;
+  /**
+   * The read capacity units for the table
+   *
+   * Must be greater than 5 and lower than 20
+   *
+   * @default 5
+   */
+  readCapacity?: number
+
 }
 
 export class HitCounter extends Construct {
 
-    /** allows accessing the counter function */
-    public readonly handler: lambda.Function;
+  /** allows accessing the counter function */
+  public readonly handler: lambda.Function;
 
-    public readonly table: dynamodb.Table;
+  public readonly table: dynamodb.Table;
 
-    constructor(scope: Construct, id: string, props: HitCounterProps) {
-        super(scope, id);
-
-        const table = new dynamodb.Table(this, 'Hits', {
-            partitionKey: {
-                name: 'path',
-                type: dynamodb.AttributeType.STRING
-            },
-            removalPolicy: cdk.RemovalPolicy.DESTROY
-        });
-
-        this.handler = new lambda.Function(this, 'HitCounterHandler', {
-            runtime: lambda.Runtime.NODEJS_16_X,
-            handler: 'hitcounter.handler',
-            code: lambda.Code.fromAsset('lambda'),
-            environment: {
-                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-                HITS_TABLE_NAME: table.tableName
-            }
-        });
-
-        table.grantReadWriteData(this.handler);
-
-        props.downstream.grantInvoke(this.handler);
+  constructor(scope: Construct, id: string, props: HitCounterProps) {
+    if (props.readCapacity !== undefined && (props.readCapacity < 5 || props.readCapacity > 20)) {
+      throw new Error('readCapacity must be greater than 5 and less than 20')
     }
+
+    super(scope, id);
+
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: {
+        name: 'path',
+        type: dynamodb.AttributeType.STRING
+      },
+      encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      readCapacity: props.readCapacity ?? 5,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'hitcounter.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
+
+    table.grantReadWriteData(this.handler);
+
+    props.downstream.grantInvoke(this.handler);
+  }
 }
--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {Template, Capture } from 'aws-cdk-lib/assertions';
 import * as CdkWorkshop from '../lib/cdk-workshop-stack';
+import {HitCounter} from "../lib/hitcounter";
 
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
@@ -15,3 +17,75 @@ test('SQS Queue and SNS Topic Created', () => {
   });
   template.resourceCountIs('AWS::SNS::Topic', 1);
 });
+
+test('DynamoDb Table Created', () => {
+  const stack = new cdk.Stack()
+
+  // WHEN
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromInline('lambda')
+    }),
+  });
+
+  // THEN
+  const template = Template.fromStack(stack);
+  template.resourceCountIs("AWS::DynamoDB::Table", 1);
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    SSESpecification: {
+      SSEEnabled: true
+    }
+  })
+});
+
+
+test('Lambda Has Environment Variables', () => {
+  const stack = new cdk.Stack();
+
+  // WHEN
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
+
+  // THEN
+  const template = Template.fromStack(stack)
+  const envCapture = new Capture()
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture
+  })
+
+  expect(envCapture.asObject()).toEqual({
+    Variables: {
+      DOWNSTREAM_FUNCTION_NAME: {
+        Ref: "TestFunction22AD90FC"
+      },
+      HITS_TABLE_NAME: {
+        Ref: "MyTestConstructHits24A357F0"
+      }
+    }
+  })
+});
+
+
+test('read capacity can be configured', () => {
+  const stack = new cdk.Stack()
+
+  expect(() => {
+    new HitCounter(stack, 'MyTestConstruct', {
+      downstream: new lambda.Function(stack, 'TestFunction', {
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: 'hello.handler',
+        code: lambda.Code.fromAsset('lambda')
+      }),
+      readCapacity: 3
+    })
+  })
+    .toThrowError(/readCapacity must be greater than 5 and less than 20/)
+})
+
